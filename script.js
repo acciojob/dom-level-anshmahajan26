792b48f5f838a2c017a01d0ea2bff4d1f04d729e
Ansh Mahajan <anshmahajan678@gmail.com>
2026-01-17 08:47:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// get the element with id "level"
+let element = document.getElementById("level");
+
+let level = 0;
+
+// count parents including html
+while (element) {
+  level++;
+  element = element.parentElement;
+}
+
+// show result
+alert("The level of the element is: " + level);
